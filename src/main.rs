4ebe288bb87650394450bbd762b1707c14fe9b4f
Robert <sehlstedt.robert@gmail.com>
2024-11-21 15:48:43 +0900

    Performs some simple instructions

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
-use cpu_6502::{cpu::{Bus, Cpu}, instruction::{AddressingMode, InstructionCode}, Variant};
+use cpu_6502::{nmos6502::Nmos6502, Bus, Cpu};
 
-struct MyCPU;
-impl Variant for MyCPU {
-    fn decode(opcode: u8) -> Option<(
-            cpu_6502::instruction::InstructionCode,
-            cpu_6502::instruction::AddressingMode
-        )> {
-        Some((InstructionCode::ADC, AddressingMode::IMM))
-    }
-}
-
-struct Memory;
+struct Memory([u8; 65536]);
 impl Bus for Memory {
-    fn read(&mut self, addr: u16) -> u8 {
-        todo!()
-    }
-
-    fn write(&mut self, addr: u16, value: u8) {
-        todo!()
-    }
+    fn read(&self, addr: u16) -> u8 { self.0[addr as usize] }
+    fn write(&mut self, addr: u16, value: u8) { self.0[addr as usize] = value }
 }
 
 fn main() {
-    // let mut c = Cpu::new(MyCPU);
-    // let mut m= Memory{};
+    let mut mem = Memory([0; 65536]);
+    mem.write(0xFFFC, 0xA0); // Set PC to A0 on reset
+
+    let mut cpu = Cpu::new(Nmos6502);
+    cpu.pc = 0x00A0;
+    cpu.sp = 0x01;
+
+    mem.write(0x00A0, 0x18); // CLC
+    cpu.reg.c = true;
+    cpu.step(&mut mem);
+
+    println!("Carry: {:?}", cpu.reg.c);
+    println!("PC: {:04x}", cpu.pc);
+
+    mem.write(0x00A1, 0x6C); // JMP IND
+    mem.write(0x00A2, 0xBB);
+    mem.write(0x00A3, 0xCC);
+
+    mem.write(0xCCBB, 0xCD);
+    mem.write(0xCCBC, 0xAB);
+
+    cpu.step(&mut mem);
+
+    println!("PC: {:04x}", cpu.pc);
+
     // c.step(&mut m);
-    let n: u8 = 0xFF;
 }
